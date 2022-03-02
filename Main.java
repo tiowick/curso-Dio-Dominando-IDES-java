40fe2253ee6d8d17ecfc42e4b51b3a0a4bc2edaf
tiowick <100328826+tiowick@users.noreply.github.com>
2022-03-02 19:15:06 -0300

    aprendendo
    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        gato gato = new gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println(" Hello js! " + (a+b));*/
+
+        
+    }
+}
